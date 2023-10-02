2c3503c12420e1b763799dd6e1ac04628ec88ecd
Ntyleen <dimon042@yandex.ru>
2023-10-02 15:43:13 +1000

    ver. 1.0

--- main.py
+++ main.py
@@ -22,25 +22,33 @@ def main():
     try:
         tables = [Cities, Products, Branches, Sales, ProductsClasses]
 
+        # Создание таблиц
         create_tables(tables)
+
+        # Загрузка данных в таблицы
         load_all_data(data_mapping)
 
+        #   Создание сессии
         sess = create_db_session()
 
+        # Выполнение запросов к бд и сохранение результатов в папку результов
         queries_to_csv(sess,
                        [get_top_branches_by_sales, get_top_branches_by_sales_value,
                         get_top_products_by_sales_at_branches,
                         get_top_products_by_sales_at_storages, get_top_cities_by_sales, get_top_sales_hour_and_day,
                         get_sales_by_day, get_sales_by_hour], column_mapping)
-
+        # создание dataframe для построения график
         df_line = queries_to_csv(sess, [get_sales_by_hour], column_mapping, False)
+        # Запуск скрипта построения графика
         vis_to_queries(df_line, "Час", "Количество_продаж", "Продаж в каждом часе")
 
         df_bar = queries_to_csv(sess, [get_sales_by_day], column_mapping, False)
         vis_to_queries(df_bar, "День_недели", "Количество_продаж", "Продаж по дням недели", "bar")
 
+        # Запуск скрипта для классификации товаров и сохранения результатов в папку результатов
         product_to_csv(sess, column_mapping["classify_products"])
 
+        # Загрузка результатов в бд
         load_classified_products()
     except Exception as e:
         logger.error(f"Ошибка при выполнении одного из модулей: {e}")
